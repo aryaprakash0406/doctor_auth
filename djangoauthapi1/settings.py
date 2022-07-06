b5823e056a440b8aa0045c6ed1decb5da043dbf5
Arya_prakash <aryaprakash0406@gmail.com>
2022-07-06 11:14:49 +0530

    d

--- djangoauthapi1/settings.py
+++ djangoauthapi1/settings.py
@@ -18,7 +18,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
